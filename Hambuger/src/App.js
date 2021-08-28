2e6f9fc5ac9617fb4c8794481226dc13d7ae5f1e
huynguyentrong <76550534+huynguyentrong@users.noreply.github.com>
2021-08-28 15:19:50 +0700

    Update App.js

--- Hambuger/src/App.js
+++ Hambuger/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import BaiTapBurger from './component/BaiTap/BaiTapBurger';
-// thằng component BaiTapBurger nó đang bị lỗi nhe e e nên viết lại thằng này thằng điên này
-
 function App() {
   return (
     <div>
